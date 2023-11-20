8d4b82d998ad82bd0b1b6cc926e3ad52666e678c
Yaroslav <yaroslavv86@gmail.com>
2023-11-20 16:40:39 +0600

    fix

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useModalStore } from "@/store/ModalStore";
+
+import Image from "next/image";
+
+export default function Home() {
+   return <main className="relative"></main>;
+}
