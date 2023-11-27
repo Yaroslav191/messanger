ca2d6fd06e6f8711a2eb0377a5f32e95f03d8c1a
Yaroslav <dunadein_5@mail.ru>
2023-11-27 23:52:14 +0600

    fix

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+'use client';
+
+import { databases } from '@/appwrite';
+import React, { useEffect, useState } from 'react';
 
 const MessageList = () => {
-   return <div>MessageList</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      const datas = await databases.listDocuments(
+        process.env.NEXT_PUBLIC_DATABASE_ID!,
+        process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      );
+      setData(datas);
+    };
+
+    getMessages();
+  }, []);
+
+  console.log(data);
+
+  return <div>MessageList</div>;
 };
 
 export default MessageList;
--- redis.ts
+++
@@ -1,6 +0,0 @@
-import Redis from 'ioredis'
-
-const redis = new Redis(process.env.GOOGLE_CLIENT_SECRET!);
-await redis.set('foo', 'bar');
-
-export default redis
