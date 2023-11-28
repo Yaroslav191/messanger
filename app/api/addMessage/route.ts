03cb36a8845a638a82682dd27c6ad5122202bd90
Yaroslav <yaroslavv86@gmail.com>
2023-11-29 00:24:30 +0600

    fix

--- app/api/addMessage/route.ts
+++ app/api/addMessage/route.ts
@@ -1,17 +1,25 @@
-import redis from "@/redis"
+import { databases } from "@/appwrite";
 
 export async function POST(req: Request, res: Response) {
-    
-    const  {newMessage} = await req.json()
+  const { newMessage } = await req.json();
 
-    console.log(newMessage)
+  console.log(newMessage);
 
-    const message = {
-        ...newMessage,
-        created_at: Date.now()
-    }
-   
-    await redis.hset('messages', message.id, JSON.stringify(message))
+  const message = {
+    ...newMessage,
+    created_at: Date.now(),
+  };
 
-    return Response.json({message})
- }
+  const documentResponse = await databases.createDocument(
+    process.env.NEXT_PUBLIC_DATABASE_ID!,
+    process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+    message.id,
+    { id: message.id, message: JSON.stringify(message) }
+  );
+
+  console.log(JSON.stringify(message));
+
+  // await redis.hset('messages', message.id, JSON.stringify(message))
+
+  return Response.json({ message });
+}
