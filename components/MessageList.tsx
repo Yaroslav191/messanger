ce498902be0dfd0f0396cb377ad7d46fa5267bdb
Yaroslav <yaroslavv86@gmail.com>
2023-12-05 00:04:56 +0600

    fix

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import { clientPusher } from '@/pusher';
-import { useSession } from 'next-auth/react';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { clientPusher } from "@/pusher";
+import { useSession } from "next-auth/react";
 
 const MessageList = ({
   messages,
@@ -13,14 +13,13 @@ const MessageList = ({
   setLoadMessages: (loadMessage: Message) => void;
 }) => {
   const { data: session, status } = useSession();
-  console.log(session?.user);
-  console.log(messages);
+
   const isUser = session?.user;
 
   useEffect(() => {
-    const channel = clientPusher.subscribe('messages');
+    const channel = clientPusher.subscribe("messages");
 
-    channel.bind('new-message', async (data: Message) => {
+    channel.bind("new-message", async (data: Message) => {
       setLoadMessages({} as Message);
     });
   }, []);
@@ -34,11 +33,13 @@ const MessageList = ({
               <li
                 key={item.id}
                 className={`flex w-full ${
-                  isUser?.email === JSON.parse(item.message).email && 'justify-end'
+                  isUser?.email === JSON.parse(item.message).email &&
+                  "justify-end"
                 }`}>
                 <div
                   className={`flex-shrink-0 ${
-                    isUser?.email === JSON.parse(item.message).email && 'order-2'
+                    isUser?.email === JSON.parse(item.message).email &&
+                    "order-2"
                   }`}>
                   <Image
                     src={JSON.parse(item.message).profileImage}
@@ -53,8 +54,8 @@ const MessageList = ({
                   <p
                     className={`text-[0.65rem] px-[2px] pb-[2px] text-red-400 ${
                       isUser?.email === JSON.parse(item.message).email
-                        ? 'text-blue-400 text-right'
-                        : 'text-red-400 '
+                        ? "text-blue-400 text-right"
+                        : "text-red-400 "
                     }`}>
                     {JSON.parse(item.message).userame}
                   </p>
@@ -63,16 +64,19 @@ const MessageList = ({
                     <div
                       className={`px-3 py-2 rounded-lg w-fit text-white bg-red-400 ${
                         isUser?.email === JSON.parse(item.message).email
-                          ? 'bg-blue-400 ml-auto order-2'
-                          : 'bg-red-400'
+                          ? "bg-blue-400 ml-auto order-2"
+                          : "bg-red-400"
                       }`}>
                       <p>{JSON.parse(item.message).message}</p>
                     </div>
                     <p
                       className={`text-[0.65rem] italic px-2 text-gray-300 ${
-                        isUser?.email === JSON.parse(item.message).email && 'text-right'
+                        isUser?.email === JSON.parse(item.message).email &&
+                        "text-right"
                       }`}>
-                      {new Date(JSON.parse(item.message).created_at).toLocaleString()}
+                      {new Date(
+                        JSON.parse(item.message).created_at
+                      ).toLocaleString()}
                     </p>
                   </div>
                 </div>
